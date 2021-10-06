289b6210866cb37cc2970488639050e5c7f3de78
sebastian-ks <seb.scheinert@gmail.com>
2021-10-06 18:16:42 +0200

    js testing

---
+++ script.js
@@ -0,0 +1,10 @@
+function t(){
+  document.getElementById("headline").innerHTML = "ERSETZT!";
+
+  //document.write("YOOOO");
+}
+function albumview(){
+  document.getElementById("albums_all").style.display = "block";
+  document.getElementById("albums_playlists").style.display = "block";
+  document.getElementById("albums_compilations").style.display = "block";
+}
